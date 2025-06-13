dc571a531b3e772cbe932d6567c10c0210abde4c
Sunny Dodti <sunnydodti@gmail.com>
2025-06-14 02:05:39 +0530

    move email host to .env

--- src/config/email.ts
+++ src/config/email.ts
@@ -4,9 +4,8 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   secure: true,
-  host: 'smtp.gmail.com',
+  host: process.env.EMAIL_HOST,
   port: 465,
-  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
